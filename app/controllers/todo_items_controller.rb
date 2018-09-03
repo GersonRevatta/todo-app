40d759bb744e8313ee62df969c75f492a4e5f4a4
gerson <gersonrevatta@gmail.com>
2018-09-03 15:48:10 -0500

    Add Todo_item

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,29 @@
+class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+  before_action :set_todo_item, only: :destroy
+  def create
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
+    if @todo_item.save
+      redirect_to @todo_list, notice: 'Successfully content created'
+    end
+  end
+
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.destroy
+    redirect_to @todo_list, notice: 'Successfully content destroyed'
+  end
+
+  private
+  def todo_item_params
+    params.require(:todo_item).permit(:content)
+  end
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+end
